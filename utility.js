1ed4ddbdfb814b9158f3e53863681b80ae442f0d
Filidor Wiese <fili@fili.nl>
2018-09-07 17:21:25 +0200

    Adds a reusable standard input Observable utility

---
+++ utility.js
@@ -0,0 +1,18 @@
+import { fromEvent } from 'rxjs'
+
+export const fromStdIn = () => {
+  console.log('Press keys...')
+  const stdin = process.stdin
+  stdin.setRawMode(true)
+  stdin.setEncoding('utf-8')
+
+  const observable = fromEvent(stdin, 'data')
+  observable.subscribe(key => {
+    if (key === '\u0003') {
+      process.exit()
+    }
+  })
+  return observable
+}
+
+// fromStdIn().subscribe(a=>console.log(a));
